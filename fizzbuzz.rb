d082415e9c95da4346b686d405ff1c5b0eb2c1db
avadavis5013 <adavis5013@gmail.com>
2018-07-11 15:28:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
-  if number % 3 == 0 
-    puts "Fizz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
+  if number % 5 == 0 && number % 3 == 0
     puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif  number % 5 == 0
+    puts "Buzz"
   else
     puts "This does not divide."
 end
